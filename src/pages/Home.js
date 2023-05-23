46f34411bf86bb92c8ccdb4c29c10801b36203b8
treflest remy <mrtreflestremy@outlook.com>
2023-05-23 15:47:26 +0200

    images incorporées + rangement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,9 @@ const pokemons = file.pokemons.map((pokemon, index) => {
     let types = pokemon.type.map((type) => type)
 
     return (
-        <tr key={pokemon.id}>
+        <tr key={pokemon.id} style={{ height: "50px" }}>
             <th scope="row">{pokemon.id}</th>
+            <td><img src={`${file.url}${pokemon.id}.png`} alt={pokemon.name.french} style={{ width: "50px", height: "50px" }} /></td>
             <td><Link to={`/pokemon/${pokemon.id}`}>{pokemon.name.french}</Link></td>
             <td>{types}</td>
         </tr>
@@ -22,6 +23,7 @@ const pokemons = file.pokemons.map((pokemon, index) => {
                 <thead>
                     <tr>
                         <th scope="col">#</th>
+                        <th scope="col">Image</th>
                         <th scope="col">Nom</th>
                         <th scope="col">Type</th>
                     </tr>
